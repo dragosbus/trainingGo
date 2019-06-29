435c33ddc70fdb9dc2c7aefe1122fde1209bae30
dragosbus <dragosbusuioc94@gmail.com>
2019-06-29 13:09:36 +0200

    numerals

---
+++ 02_numerals/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	//print a decimal, it's binary format and hexa format
+	fmt.Printf("%d - %b - %x \n", 42, 42, 42)
+	fmt.Printf("%d - %#x \n", 42, 42)
+}
