2396061d5bfddd812c92b071ad427252407f25d8
dragosbus <dragosbusuioc94@gmail.com>
2019-07-06 08:14:42 +0200

    variables

---
+++ 05_variables/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := "golang"
+	c := 4.17
+	d := true
+	var e int
+	var f string
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+	fmt.Printf("%v \n", e)
+	fmt.Printf("%v \n", f)
+}
